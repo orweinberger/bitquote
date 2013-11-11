62d187de72f9631f9a4b9366258e5de0cbe9f24b
orjoola <or@joo.la>
2013-11-11 16:17:50 +0200

    Some progress, still not functional

---
+++ README.md
@@ -0,0 +1,4 @@
+##BitQuote
+####Not functional yet - please do not use
+
+An easy to embed bitcoin to fiat widget
---
+++ index.js
@@ -0,0 +1,11 @@
+var options = {"fiat":"USD", "fiatSymbol": "$"}
+var $ = require('jquery');
+exports.init = function(container) {
+  if (typeof something === "undefined") {
+    console.log('container not defined, initializing to body')
+    var container = window.$(body);
+    container.text('asd');
+  }
+}
+
+
