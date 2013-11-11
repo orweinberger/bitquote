16c79ce5c4d345c4ba9e6e6b8c4325839858c03e
orweinberger <orweinberger@gmail.com>
2013-11-11 18:38:38 +0200

    All is now functional, minor css defects

--- bitquote.js
+++ bitquote.js
@@ -3,9 +3,14 @@ function initialize(options) {
     var container = $('#' + options.container);
     $('<div class="bitquote-logo"><img src="https://en.bitcoin.it/w/images/en/2/29/BC_Logo_.png" width="85" /></div>').appendTo(container);
     $('<div class="bitquote-price"></div>').appendTo(container);
-    $('<div class="bitquote-ask"></div>').appendTo(container);
-    $('<div class="bitquote-bid"></div>').appendTo(container);
+    var askbidParent = $('<div class="askbidParent"></div>');
+    $(askbidParent).appendTo(container);
+    $('<div class="bitquote-price"></div>');
+    $('<div class="bitquote-ask"></div>').appendTo(askbidParent);
+    $('<div class="bitquote-bid"></div>').appendTo(askbidParent);
     $('<div class="clearboth"></div>').appendTo(container);
     $(".bitquote-price").html(options.fiatSymbol + data.last);
+    $(".bitquote-bid").html("Bid: " + options.fiatSymbol + data.bid);
+    $(".bitquote-ask").html("Ask: " + options.fiatSymbol + data.ask);
   });
 }
